fb2310c32ea2744b6fc304bfb912b0d906072886
Pranjal <pranjalmaurya01@gmail.com>
2024-02-19 13:12:30 +0530

    fix: captcha during tatkal

--- go.mod
+++ go.mod
@@ -3,17 +3,33 @@ module irctc
 go 1.22.0
 
 require (
+	cloud.google.com/go/vision v1.2.0
 	github.com/chromedp/chromedp v0.9.5
 	github.com/joho/godotenv v1.5.1
 )
 
 require (
+	cloud.google.com/go v0.100.2 // indirect
+	cloud.google.com/go/compute v1.3.0 // indirect
 	github.com/chromedp/cdproto v0.0.0-20240214232516-ad4608604e9e // indirect
 	github.com/chromedp/sysutil v1.0.0 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.7 // indirect
+	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
 	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/api v0.70.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220222213610-43724f9ea8cf // indirect
+	google.golang.org/grpc v1.44.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 )
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"time"
 
+	vision "cloud.google.com/go/vision/apiv1"
 	"github.com/chromedp/chromedp"
 	"github.com/chromedp/chromedp/kb"
 	"github.com/joho/godotenv"
@@ -33,21 +34,70 @@ var opts = append(chromedp.DefaultExecAllocatorOptions[:],
 	chromedp.WindowSize(1350, 1000),
 )
 
-func main() {
-	godotenv.Load()
-	username := os.Getenv("USERNAME")
-	password := os.Getenv("PASSWORD")
+var captcha_count = 0
 
-	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
-	defer cancel()
-	ctx, cancel := chromedp.NewContext(allocCtx, chromedp.WithLogf(log.Printf))
-	defer cancel()
-
+func SolveCaptcha(ctx context.Context) {
+	captcha_count++
 	var ok bool
 	var captcha_base64 string
 	chromedp.Run(ctx,
-		chromedp.Navigate("https://www.irctc.co.in/nget/train-search"),
+		chromedp.WaitVisible("img.captcha-img"),
+		chromedp.AttributeValue(`img.captcha-img`, "src", &captcha_base64, &ok),
+	)
 
+	// save captcha in png file
+	png := strings.Split(captcha_base64, ",")[1]
+	imageData, _ := base64.StdEncoding.DecodeString(png)
+	os.WriteFile("temp.png", imageData, 0644)
+
+	var output string
+
+	if captcha_count < 2 {
+		cmd := exec.Command("gocr", "temp.png")
+		out, cmd_err := cmd.CombinedOutput()
+		output = string(out)
+		if cmd_err != nil {
+			log.Fatal("please install `gocr` package")
+		}
+	} else if captcha_count >= 2 {
+		out, err := detectText()
+		if err != nil {
+			fmt.Println(err)
+		}
+		output = out
+	} else if captcha_count > 4 {
+		panic("Unable to solve captcha")
+	}
+
+	fmt.Println("captcha", captcha_count, output)
+	// Login
+	if err := chromedp.Run(ctx,
+		chromedp.WaitVisible("img.captcha-img"),
+		chromedp.Focus(`input#captcha`),
+		chromedp.SendKeys(`input#captcha`, output, chromedp.ByQuery),
+		chromedp.KeyEvent(kb.Enter),
+		// chromedp.WaitNotVisible(`div.my-loading.ng-star-inserted`),
+	); err != nil {
+		fmt.Println(err)
+	}
+
+	var is_err string
+	chromedp.Run(ctx,
+		chromedp.InnerHTML(`div.loginError`, &is_err),
+	)
+
+	if is_err == "Invalid Captcha...." {
+		SolveCaptcha(ctx)
+	} else if is_err == "Invalid User" {
+		FillLoginForm(ctx)
+	}
+}
+
+func FillLoginForm(ctx context.Context) {
+	username := os.Getenv("USERNAME")
+	password := os.Getenv("PASSWORD")
+
+	chromedp.Run(ctx,
 		// LOGIN
 		chromedp.WaitVisible("a.loginText"),
 		chromedp.Click("a.loginText", chromedp.ByQuery),
@@ -55,27 +105,26 @@ func main() {
 		chromedp.SetValue(`input[formcontrolname="userid"]`, username, chromedp.ByQuery),
 		chromedp.SetValue(`input[formcontrolname="password"]`, password, chromedp.ByQuery),
 		chromedp.SetValue(`input[formcontrolname="password"]`, password, chromedp.ByQuery),
-		chromedp.AttributeValue(`img.captcha-img`, "src", &captcha_base64, &ok),
 	)
 
-	// save capcha in png file
-	png := strings.Split(captcha_base64, ",")[1]
-	imageData, _ := base64.StdEncoding.DecodeString(png)
-	os.WriteFile("temp.png", imageData, 0644)
+	SolveCaptcha(ctx)
+}
 
-	cmd := exec.Command("gocr", "temp.png")
-	output, cmd_err := cmd.CombinedOutput()
-	if cmd_err != nil {
-		log.Fatal("please install `gocr` package")
-	}
-	capcha_val := string(output)
+func main() {
+	godotenv.Load()
 
-	// Login
-	if err := chromedp.Run(ctx,
-		chromedp.SendKeys(`input#captcha`, capcha_val, chromedp.ByQuery),
-	); err != nil {
-		fmt.Println(err)
-	}
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancel()
+	ctx, cancel := chromedp.NewContext(allocCtx, chromedp.WithLogf(log.Printf))
+	defer cancel()
+
+	chromedp.Run(ctx,
+		chromedp.Navigate("https://www.irctc.co.in/nget/train-search"),
+	)
+
+	FillLoginForm(ctx)
+
+	fmt.Println("filled")
 
 	if err := chromedp.Run(ctx,
 		// ORIGIN SET
@@ -110,5 +159,36 @@ func main() {
 	); err != nil {
 		fmt.Println(err)
 	}
+
+}
+
+func detectText() (string, error) {
+	ctx := context.Background()
+
+	client, err := vision.NewImageAnnotatorClient(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	f, err := os.Open("temp.png")
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	image, err := vision.NewImageFromReader(f)
+	if err != nil {
+		return "", err
+	}
+	annotations, err := client.DetectTexts(ctx, image, nil, 1)
+	if err != nil {
+		return "", err
+	}
+
+	if len(annotations) > 0 {
+		return annotations[0].Description, nil
+	}
+
+	return "", nil
 
 }
